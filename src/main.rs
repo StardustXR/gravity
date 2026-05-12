574bbe0c78f332061f6a276fc0aaa6e416207ffe
Schmarni <marnistromer@gmail.com>
2026-05-12 10:59:48 +0200

    refactor: update to binder based stardust
    Signed-off-by: Schmarni <marnistromer@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ use clap::Parser;
 use glam::Quat;
 use stardust_xr_fusion::{
 	client::Client,
-	root::{ClientState, RootAspect},
-	spatial::{Spatial, Transform},
+	spatial::{Spatial, SpatialExt, Transform},
 };
 use std::ffi::CString;
 use ustr::ustr;
@@ -26,35 +25,24 @@ struct Args {
 #[tokio::main(flavor = "current_thread")]
 async fn main() {
 	let args = dbg!(Args::parse());
-	let client = Client::connect()
+	let (client, root) = Client::auto_connect(&[])
 		.await
 		.expect("Unable to connect to server");
-	let spatial = Spatial::create(
-		client.get_root(),
+	let spatial = Spatial::new(
+		&client,
+		&root,
 		Transform::from_translation_rotation(
 			[args.x, args.y, args.z],
 			Quat::from_rotation_y(args.yaw.unwrap_or_default().to_radians()),
 		),
-		false,
 	)
+	.await
 	.unwrap();
+	let spatial_ref = spatial.spatial_ref().await.unwrap();
 
-	let client_handle = client.handle();
-	let _async_loop = client.async_event_loop();
-
-	let env = client_handle
-		.get_root()
-		.get_connection_environment()
-		.await
-		.expect("Server could not get the environment needed to connect to stardust");
-	for (k, v) in env.into_iter() {
-		println!("Setting connection env var {k} to {v}");
-		std::env::set_var(k, v);
-	}
-
-	let startup_token = client_handle
-		.get_root()
-		.generate_state_token(ClientState::from_root(&spatial).unwrap())
+	let startup_token = client
+		.server()
+		.generate_startup_token(spatial_ref)
 		.await
 		.expect("Server could not generate startup token");
 	std::env::set_var("STARDUST_STARTUP_TOKEN", startup_token);
